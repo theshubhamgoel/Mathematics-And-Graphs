6a5d031a0808079a5fc0da0fc5ee2bb239853088
Goel, Shubham <shubhgoe@amazon.com>
2017-12-16 23:05:01 +0530

    Modular Multiplication Around a Circle

---
+++ WebContent/AllJS/basic.js
@@ -0,0 +1,14 @@
+function getScreenWidth() {
+	var width = window.innerWidth || document.documentElement.clientWidth
+			|| document.body.clientWidth;
+
+	return width;
+}
+
+function getScreenHeight() {
+
+	var height = window.innerHeight || document.documentElement.clientHeight
+			|| document.body.clientHeight;
+
+	return height
+}
